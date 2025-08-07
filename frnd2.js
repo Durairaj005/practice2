999463b5a2696d1a76c42429150457f1dd23893a
DURAIRAJAN G <2303737724321011@ksrct.net>
2025-08-07 20:49:03 +0530

    feat: some change in js

--- frnd2.js
+++ frnd2.js
@@ -1,2 +1,4 @@
 console.log("Hello, world! This is a sample JavaScript program.");
-console.log("This file is located at: practice2/frnd2.js"); 
+console.log("Let's add some more lines to this program.");
+console.log("JavaScript makes it easy to print messages.");
+console.log("End of sample program.");
